f72d453341feecd5f689cbab1971f51b93de3148
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2023-08-14 21:46:48 +0000

    stuff

--- tezos-rpc/src/models/constants.rs
+++ tezos-rpc/src/models/constants.rs
@@ -4,7 +4,7 @@ use {
     serde::{Deserialize, Serialize},
 };
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Constants {
     /// integer ∈ [0, 255]
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -304,7 +304,7 @@ pub struct Constants {
     pub sc_rollup_max_available_messages: Option<i32>,
 }
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub struct RatioConstant {
     /// integer ∈ [0, 2^16-1]
     numerator: u16,
